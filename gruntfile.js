50a64a13a0aad17e8d3c2df4973d1742f990dd74
aestrada7 <vanhelsing44@gmail.com>
2016-04-29 19:30:03 -0500

    Initial Project Setup

---
+++ gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+  
+  grunt.initConfig({
+    watch: {
+      css: {
+        files: ['app/features/**/*.scss', 'app/styles/*.scss', 'app/styles/components/*.scss', 'app/components/**/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+
+    sass: {
+      build: {
+        files: [
+          {
+            expand: true,
+            cwd: 'app',
+            src: ['features/**/*.scss', 'styles/*.scss'],
+            ext: '.css',
+            dest: 'app/build'
+          }
+        ]
+      }
+    },
+
+    clean: {
+      bower: ['bower_components'],
+      vendor: ['app/vendor'],
+      develop: ['app/build']
+    },
+
+    bower: {
+      install: {
+        options: {
+          targetDir: './app/vendor',
+          cleanTargetDir: true,
+          layout: 'byComponent'
+        }
+      }
+    },
+
+    parallel: {
+      watchers: {
+        tasks: [{
+          //grunt: true,
+          //args: ['shell:nw']
+        }, {
+          stream: true,
+          args: ['watch']
+        }]
+      }
+    }
+  });
+
+  grunt.registerTask('clean-instance', ['clean:bower', 'clean:vendor', 'clean:develop']);
+  grunt.registerTask('update', ['clean-instance', 'bower']);
+  grunt.registerTask('develop', ['sass', 'parallel:watchers']);
+};
